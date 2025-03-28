15dccb75c4f8acb6362baa2b80523f081cd56978
agamdana17 <agamdana125@gmail.com>
2025-03-28 18:39:53 +0700

    memperbaiki navbar

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,15 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view(view: 'home');
 });
+
+Route::get('/shop', function () {
+    return view(view: 'shop');
+});
+
+Route::get('/report', function () {
+    return view(view: 'report');
+});
+
+Route::get('/life', function () {
+    return view(view: 'life');
+});
